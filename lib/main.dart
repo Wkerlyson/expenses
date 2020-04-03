5507c8757aac377807a044a838799f70ebc31f1a
Wkerlyson <wkerlyson.batista@gmail.com>
2020-04-03 12:45:45 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Versão inicial'),
+    );
+  }
+}
