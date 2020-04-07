9fe682472ea4e6726bd8a177ef279222bce1f069
Wkerlyson <wkerlyson.batista@gmail.com>
2020-04-07 10:32:58 -0300

    displaying Datepicker

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class TransactionForm extends StatefulWidget {
   final void Function(String, double) onSubmit;
@@ -10,19 +11,38 @@ class TransactionForm extends StatefulWidget {
 }
 
 class _TransactionFormState extends State<TransactionForm> {
-  final titleController = TextEditingController();
-
-  final valueController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _valueController = TextEditingController();
+  DateTime _selectedDate;
 
   _submitForm() {
-    final title = titleController.text;
-    final value = double.tryParse(valueController.text) ?? 0.0;
+    final title = _titleController.text;
+    final value = double.tryParse(_valueController.text) ?? 0.0;
 
     if (title.isEmpty || value <= 0) {
       return;
     }
 
     widget.onSubmit(title, value);
+  }
+
+  _showDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2019),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+
+    print("Executado");
   }
 
   @override
@@ -38,7 +58,7 @@ class _TransactionFormState extends State<TransactionForm> {
               color: Colors.grey.shade100,
             ),
             child: TextField(
-              controller: titleController,
+              controller: _titleController,
               onSubmitted: (_) => _submitForm(),
               cursorColor: Theme.of(context).primaryColor,
               decoration: InputDecoration(
@@ -54,7 +74,7 @@ class _TransactionFormState extends State<TransactionForm> {
               color: Colors.grey.shade100,
             ),
             child: TextField(
-              controller: valueController,
+              controller: _valueController,
               onSubmitted: (_) => _submitForm(),
               cursorColor: Theme.of(context).primaryColor,
               keyboardType: TextInputType.numberWithOptions(decimal: true),
@@ -65,11 +85,32 @@ class _TransactionFormState extends State<TransactionForm> {
               ),
             ),
           ),
+          Container(
+            height: 60,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    _selectedDate == null
+                        ? 'Nenhuma data selecionada '
+                        : 'Data selecionada: ${DateFormat('dd/MM/y').format(_selectedDate)}',
+                  ),
+                ),
+                FlatButton(
+                  onPressed: _showDatePicker,
+                  child: Text('Selecionar data'),
+                  textColor: Theme.of(context).accentColor,
+                )
+              ],
+            ),
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
-              FlatButton(
-                textColor: Theme.of(context).accentColor,
+              RaisedButton(
+                elevation: 0,
+                textColor: Colors.white,
+                color: Theme.of(context).primaryColor,
                 child: Text(
                   'SALVAR',
                   style: TextStyle(
